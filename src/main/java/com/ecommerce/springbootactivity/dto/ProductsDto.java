acdaa1f7248287855f891bb15387489d5bf99144
sammuel.tuliao@novare.com.hk <sammuel.tuliao@novare.com.hk>
2023-01-18 09:38:14 +0800

    added products list

---
+++ src/main/java/com/ecommerce/springbootactivity/dto/ProductsDto.java
@@ -0,0 +1,63 @@
+package com.ecommerce.springbootactivity.dto;
+
+public class ProductsDto {
+
+    private int productId;
+
+
+    private String productName;
+    private String productDescription;
+    private int productQuantity;
+
+    private double productPrice;
+
+
+    private int userId;
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(int productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
